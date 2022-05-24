4c7e8d8011786865cce5c2241aebd1346f9aff4e
Michael Berghold <michael.berghold@oewf.org>
2022-05-24 17:23:51 +0200

    Refined capture_frame.py

--- capture_frame.py
+++ capture_frame.py
@@ -1,3 +1,59 @@
-import numpy as np
-import rawpy
+from subprocess import Popen, PIPE
 import matplotlib.pyplot as plt
+import rawpy
+import io
+
+brightness = 135                                   # min=0 max=255 step=1 default=135
+contrast = 35                                     # min=0 max=95 step=1 default=35
+saturation = 100                                 # min=0 max=100 step=1 default=40
+hue = 0                                          # min=-2000 max=2000 step=100 default=0
+white_balance_temperature_auto = 0               # default=1
+white_balance_temperature = 4600                 # min=2800 max=6500 step=1 default=4600
+gamma = 140                                      # min=100 max=300 step=1 default=140
+gain = 255                                       # min=16 max=255 step=1 default=16
+power_line_frequency = 0                         # min=0 max=2 default=1 (0: Disabled / 1: 50 Hz / 2: 60 Hz)
+sharpness = 5                                    # min=0 max=70 step=1 default=5
+backlight_compensation = 64                       # min=8 max=200 step=1 default=64
+exposure_auto = 1                                 # min=0 max=3 default=3 (1: Manual Mode / 3: Aperture Priority Mode)
+exposure_absolute = 8192                          # min=3 max=8192 step=1 default=500
+
+cmd = ['ssh',
+       'experiment',
+       'v4l2-ctl',
+       '--device=/dev/video4',
+
+       '--set-fmt-video=' +
+       f'width={1920},' +
+       f'height={1080},' +
+       'pixelformat=YUYV',
+
+       '--set-ctrl=' +
+       f'brightness={brightness},' +
+       f'contrast={contrast},' +
+       f'saturation={saturation},' +
+       f'hue={hue},' +
+       f'white_balance_temperature_auto={white_balance_temperature_auto},' +
+       f'gamma={gamma},' +
+       f'gain={gain},' +
+       f'power_line_frequency={power_line_frequency},' +
+       f'white_balance_temperature={white_balance_temperature},' +
+       f'sharpness={sharpness},' +
+       f'backlight_compensation={backlight_compensation},' +
+       f'exposure_auto={exposure_auto},' +
+       f'exposure_absolute={exposure_absolute}',
+
+       '--stream-mmap',
+       '--stream-count=1',
+       '--stream-to=-']
+
+process = Popen(cmd, stdout=PIPE, stderr=PIPE)
+stdout, stderr = process.communicate()
+
+img = rawpy.imread(io.BytesIO(stdout))
+
+img_monochrome = img.raw_image_visible
+
+print(img_monochrome[50,50])
+
+plt.imshow(img_monochrome, cmap='gray', interpolation=None)
+plt.show()
--- open_raw.py
+++ open_raw.py
@@ -1,14 +1,21 @@
 import numpy as np
 import rawpy
 import matplotlib.pyplot as plt
-
-img = rawpy.imread('/Users/bergi/test_m/file.raw')
-
+import io
 
 
-#%%
+def split_list(alist, n):
+    length = len(alist)
+    return [alist[i*length // n: (i+1)*length // n] for i in range(n)]
+
+
+with open('/Users/bergi/test_m/file.raw', 'rb') as f:
+    data = f.read()
+
+data = split_list(data, 3)[0]
+img = rawpy.imread(io.BytesIO(data))
+
 img_monochrome = img.raw_image_visible
-#img_proc = img.postprocess()
 
 plt.imshow(img_monochrome, cmap='gray', interpolation=None)
 plt.show()
@@ -16,7 +23,7 @@ plt.show()
 exit()
 
 
-#%%
+
 img_proc = img.postprocess()
 img_proc = img_proc.reshape(1080*1920, 3)
 
