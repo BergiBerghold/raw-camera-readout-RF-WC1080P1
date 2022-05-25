e0abc59c8d75ea889b37d165438f43212295a706
Michael Berghold <michael.berghold@oewf.org>
2022-05-25 15:16:39 +0200

    Added camera settings

--- capture_frame.py
+++ capture_frame.py
@@ -1,21 +1,39 @@
 from subprocess import Popen, PIPE
 import matplotlib.pyplot as plt
+import numpy as np
 import rawpy
 import io
 
-brightness = 135                                   # min=0 max=255 step=1 default=135
-contrast = 35                                     # min=0 max=95 step=1 default=35
+
+def split_list(alist, n):
+    length = len(alist)
+    return [alist[i*length // n: (i+1)*length // n] for i in range(n)]
+
+
+# Capture settings
+
+number_of_frames = 5
+resolution = 1920, 1080
+
+# Camera settings
+# Some effect
+
 saturation = 100                                 # min=0 max=100 step=1 default=40
 hue = 0                                          # min=-2000 max=2000 step=100 default=0
 white_balance_temperature_auto = 0               # default=1
 white_balance_temperature = 4600                 # min=2800 max=6500 step=1 default=4600
-gamma = 140                                      # min=100 max=300 step=1 default=140
-gain = 255                                       # min=16 max=255 step=1 default=16
+exposure_auto = 3                                # min=0 max=3 default=3 (1: Manual Mode / 3: Aperture Priority Mode)
+
+# No effect
+
+contrast = 95                                    # min=0 max=95 step=1 default=35
+brightness = 255                                 # min=0 max=255 step=1 default=135
+gamma = 100                                      # min=100 max=300 step=1 default=140
+gain = 16                                        # min=16 max=255 step=1 default=16
 power_line_frequency = 0                         # min=0 max=2 default=1 (0: Disabled / 1: 50 Hz / 2: 60 Hz)
-sharpness = 5                                    # min=0 max=70 step=1 default=5
-backlight_compensation = 64                       # min=8 max=200 step=1 default=64
-exposure_auto = 1                                 # min=0 max=3 default=3 (1: Manual Mode / 3: Aperture Priority Mode)
-exposure_absolute = 8192                          # min=3 max=8192 step=1 default=500
+sharpness = 0                                    # min=0 max=70 step=1 default=5
+backlight_compensation = 8                       # min=8 max=200 step=1 default=64
+exposure_absolute = 3                            # min=3 max=8192 step=1 default=500
 
 cmd = ['ssh',
        'experiment',
@@ -23,8 +41,8 @@ cmd = ['ssh',
        '--device=/dev/video4',
 
        '--set-fmt-video=' +
-       f'width={1920},' +
-       f'height={1080},' +
+       f'width={resolution[0]},' +
+       f'height={resolution[1]},' +
        'pixelformat=YUYV',
 
        '--set-ctrl=' +
@@ -43,17 +61,27 @@ cmd = ['ssh',
        f'exposure_absolute={exposure_absolute}',
 
        '--stream-mmap',
-       '--stream-count=1',
+       f'--stream-count={number_of_frames}',
        '--stream-to=-']
 
 process = Popen(cmd, stdout=PIPE, stderr=PIPE)
 stdout, stderr = process.communicate()
 
-img = rawpy.imread(io.BytesIO(stdout))
+frames = split_list(stdout, number_of_frames)
+sum_of_frames = np.zeros(resolution[::-1])
 
-img_monochrome = img.raw_image_visible
+for n, frame in enumerate(frames):
+    with rawpy.imread(io.BytesIO(frame)) as img:
+        img_monochrome = img.raw_image_visible
+        sum_of_frames += img_monochrome
 
-print(img_monochrome[50,50])
+        #print(f'Minimum: {np.min(img_monochrome)}')
+        #print(f'Maximum: {np.max(img_monochrome)}')
 
-plt.imshow(img_monochrome, cmap='gray', interpolation=None)
-plt.show()
+        #plt.imshow(img_monochrome, cmap='gray', interpolation=None, resample=False) #, vmin=0, vmax=65535)
+        #plt.title(f'Frame No. {n+1}')
+        #plt.show()
+
+plt.imshow(sum_of_frames, cmap='gray', interpolation=None, resample=False) #, vmin=0, vmax=65535)
+plt.title('Sum of all Frames')
+plt.savefig('sum.png', format="png", dpi=1000)
