9598c0ca91dcbeb775b06277cb23b34e777778ce
Michael Berghold <michael.berghold@oewf.org>
2022-06-01 22:07:15 +0200

    Trying with shell=True

--- plot_from_file.py
+++ plot_from_file.py
@@ -2,7 +2,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('/Users/bergi/test_m/raw-webcam-readout/measurements/run_21:54:06_31.05./datapoints.csv')
+measurement_dir = '/Users/bergi/test_m/raw-webcam-readout/measurements/run_21:54:06_31.05.'
+
+df = pd.read_csv(f'{measurement_dir}/datapoints.csv')
 data_array = np.array(df)
 
 data_array[:,0] *= 6.24150907 * 10**18
@@ -15,7 +17,10 @@ plt.plot(data_array[:,0], data_array[:,4], label='Max')
 plt.legend(loc="upper left")
 plt.xlabel('Photons/s on entire sensor')
 
-plt.annotate('Note added', xy = (0.5, -0.2), xycoords='axes fraction')
+info_text = f'Measurement directory: run_21:54:06_31.05.\n' \
+            f''
+
+plt.annotate(info_text, xy = (0.5, -0.2), xycoords='axes fraction', horizontalalignment='center')
 
 fig.tight_layout()
 plt.show()
