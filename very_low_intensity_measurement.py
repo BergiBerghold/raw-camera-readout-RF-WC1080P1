af0a33d228ac8ea0fc01e02a15315837227eb47f
Michael Berghold <michael.berghold@oewf.org>
2022-06-25 21:31:02 +0200

    Changed parameters

--- very_low_intensity_measurement.py
+++ very_low_intensity_measurement.py
@@ -9,16 +9,11 @@ import time
 import sys
 import os
 
-start=time.perf_counter()
-frame = acquire_series_of_frames(2, override_gain=255, override_brightness=255)[1]
-print(time.perf_counter() - start)
-exit()
-
 
 # User Settings
 
 intensity_increment = 1                     # [DAC steps]
-max_intensity = 5000                        # [DAC steps]
+max_intensity = 1000                        # [DAC steps]
 led_response_time = 5                       # [seconds]
 gain = 255
 brightness = 255
@@ -26,7 +21,7 @@ brightness = 255
 
 # Calculate and print execution time
 
-time_to_take_single_frame = 1
+time_to_take_single_frame = 1.2
 number_of_intensity_steps = len(range(0, max_intensity + 1, intensity_increment))
 est_execution_time = number_of_intensity_steps * ( led_response_time + time_to_take_single_frame)
 
