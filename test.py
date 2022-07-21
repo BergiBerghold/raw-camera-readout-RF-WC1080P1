9e1ce590e408edc2014f770c4b5624e55c4bc409
Michael Berghold <michael.berghold@oewf.org>
2022-07-21 10:45:07 +0200

    Testing temp controller

--- test.py
+++ test.py
@@ -3,12 +3,11 @@ import os
 
 
 get_temp_cmd = ['ssh', '-tt', 'experiment', 'TempReadout']
-shell = False
 
 if os.getenv('CCD_MACHINE'):
     get_temp_cmd = get_temp_cmd[3:]
 
-with Popen(get_temp_cmd, stdout=PIPE, stderr=DEVNULL, bufsize=0) as p:
+with Popen(get_temp_cmd, stdout=PIPE, stderr=DEVNULL, bufsize=1, universal_newlines=True) as p:
     for line in p.stdout:
         print(line)
         try:
